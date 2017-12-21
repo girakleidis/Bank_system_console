cf91040c5efd9c5cc841773c864bd63eed89307b
George <George@George-PC>
2017-12-21 04:17:09 +0200

    2nd

--- src/feb_project/LoginScreen.java
+++ src/feb_project/LoginScreen.java
@@ -5,7 +5,11 @@
  */
 package feb_project;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -14,15 +18,11 @@ import java.util.Scanner;
 public class LoginScreen {
 
     private String loggedUser;
+    // 1 for normal user 2 for Admin user
     private int loggedUserLevel;
-    private DBAccess dba;
+    private DBAccess dba = new DBAccess();
 
-    public void enterCredentials(Scanner sc) {
-
-    }
-
-    public LoginScreen(int loggedUserLevel) {
-        this.loggedUserLevel = loggedUserLevel;
+    public LoginScreen() {
     }
 
     public String getLoggedUser() {
@@ -33,4 +33,30 @@ public class LoginScreen {
         return loggedUserLevel;
     }
 
+    public void welcomeScreen(Scanner sc) throws Exception {
+        boolean correctCredentials = false;
+        String tempUsername = "";
+        String tempPassword = "";
+
+        while (correctCredentials == false) {
+            System.out.println("Please give Credentials");
+
+            System.out.print("Username: ");
+            tempUsername = sc.nextLine();
+            System.out.print("Password: ");
+            tempPassword = sc.nextLine();
+
+            ArrayList al = new ArrayList();
+            al = dba.readDataBase("select username from users where username='" + tempUsername + "'" + "AND password='" + tempPassword + "'");
+            if (al.size() != 0) {
+                correctCredentials = true;
+            }
+        }
+        this.loggedUser = tempUsername;
+        if (this.loggedUser.equals("admin")) {
+            this.loggedUserLevel = 2;
+        } else {
+            this.loggedUserLevel = 1;
+        }
+    }
 }
